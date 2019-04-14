b79db28e52292366a7d8de226d11c15b978f7aa4
muammar <muammararif007@gmail.com>
2019-04-14 15:59:11 +0530

    add login status to header

--- header.php
+++ header.php
@@ -1,5 +1,15 @@
 <link rel="stylesheet" href="style.css">
 <header>
     <a href="index.php">Home</a>
+    |
+    <small><?php
+    if (isset($_SESSION["logged_in"]))  { ?>
+        Logged in as <b><?= $_SESSION["logged_in"] ?></b>
+        |
+        <a href="logout.php">Logout</a>
+    <?php
+    } else { ?>
+        <a href='login.php'>Login</a>
+    <?php } ?></small>
 </header>
 <hr>
